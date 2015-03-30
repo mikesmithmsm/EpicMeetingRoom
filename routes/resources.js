dbeba5a2c71b3a7229080d5607143919a9377a35
Michael Smith <michaelsmith@Michaels-MacBook-Pro.local>
2015-03-30 15:55:20 +0100

    for michale quayle esq to finish we're going home

--- routes/resources.js
+++ routes/resources.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Resources = require('../models/resources');
-var ical = require('ical')
+var icalendar = require('icalendar')
+var request = require('request');
 
 router.route('/:id').get(function(req, res, next) {
   Resources.findOne({id: req.params.id}, function(err, resource) {
@@ -9,29 +10,42 @@ router.route('/:id').get(function(req, res, next) {
       return res.send(err);
     }
 
-    
+    request(resource.url, function (error, response, data) {
+      var ical = icalendar.parse_calendar(data);
+      console.log('parsed' +ical.events().length  )
+      res.render('resource', {events: ical.events()});
+      events = ical.events();
 
-    ical.fromURL(resource.url, {}, function(err, data) {
-      events = [];
-      for (var k in data){
-      if (data.hasOwnProperty(k)) {
-          var ev = data[k]
-          events.push(new CalendarEntry(ev));
-        }
+
+      now = findEvent(events, new Date(), plusOneMinute(new Date()));
+
+      console.log('got now '+now.getPropertyValue('SUMMARY'))
+
+      if (now != null) {
+        var dtend = now.getPropertyValue('DTEND');
+        dtend = new Date(dtend.valueOf());
+        next = findEvent(events, dtend, plusOneMinute(dtend));
+
+        console.log('got next '+next.getPropertyValue('SUMMARY'))
       }
-      console.log(events.length+' '+events[0].summary)
-      res.render('resource', {events: events});
-    });
+      
+    })
   });
 });
 
-CalendarEntry = function(event) {
-  this.event = JSON.stringify(event);
-  this.summary = event.summary;
-  this.organiser = null;
-  this.startTime = event.start;
-  this.endTime = event.end
+function plusOneMinute(time) {
+  return time.setTime(time.getTime() + 1000 * 60);
 }
 
+function findEvent(events, startTime, endTime) {
+  var toReturn = null;
+  events.forEach(function(event) {
+    if (event.inTimeRange(startTime, endTime)) {
+      toReturn =  event;
+    }
+  });
+
+  return toReturn;
+}
 
 module.exports = router;
