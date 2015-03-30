d495bd9e687285d550907997894456fc9f043165
YOURNAME <YOUREMAIL>
2015-03-30 19:29:12 +0100

    hoorah

--- routes/resources.js
+++ routes/resources.js
@@ -12,40 +12,75 @@ router.route('/:id').get(function(req, res, next) {
 
     request(resource.url, function (error, response, data) {
       var ical = icalendar.parse_calendar(data);
-      console.log('parsed' +ical.events().length  )
       res.render('resource', {events: ical.events()});
       events = ical.events();
 
+      var currentDateTime = new Date( 2015, 2, 30, 08, 15, 0, 0 )
+      var endOfDayDateTime = new Date( 2015, 2, 30, 23, 25, 0, 0 );
+      var currentEvent = null;
+      var nextEvent = null;
+      var currentIndex = 0;      
+      var found = false;
 
-      now = findEvent(events, new Date(), plusOneMinute(new Date()));
+      eventsInRange = findEvent(events, currentDateTime, endOfDayDateTime);
 
-      console.log('got now '+now.getPropertyValue('SUMMARY'))
+      eventsInRange.forEach(function(event, index) {
+        if (event.inTimeRange(currentDateTime, plusOneMinute(currentDateTime))) {
+          currentIndex = index;
+          found = true;
+          currentEvent = event;
+        }
+      });
 
-      if (now != null) {
-        var dtend = now.getPropertyValue('DTEND');
-        dtend = new Date(dtend.valueOf());
-        next = findEvent(events, dtend, plusOneMinute(dtend));
-
-        console.log('got next '+next.getPropertyValue('SUMMARY'))
+      if (!found) {
+        eventsInRange = findEvent(events, currentDateTime, endOfDayDateTime);
+        if (eventsInRange.length > 0) {
+          nextEvent = eventsInRange[0];
+        }
+      } else {
+        nextEvent = eventsInRange[currentIndex+1];
       }
-      
+
+      console.log('>> NOW is '+currentEvent+', NEXT is '+nextEvent);
+
+      res.render('resource', {now: currentEvent, next: nextEvent, resource: resource});
     })
   });
 });
 
+function printEvent(eventName, event) {
+  if(event != null){
+    console.log(eventName + " = "+event.getPropertyValue("SUMMARY") + ", startDate id "+event.getPropertyValue('DTSTART') + ", endDate is "+event.getPropertyValue('DTEND') );  
+  }else{
+    console.log(eventName + " is NULL");
+  }
+}
+
 function plusOneMinute(time) {
-  return time.setTime(time.getTime() + 1000 * 60);
+  t = time.getTime();
+  return new Date(t + (1000 * 60));
 }
 
 function findEvent(events, startTime, endTime) {
-  var toReturn = null;
+  var eventsInDateRange = [];
   events.forEach(function(event) {
     if (event.inTimeRange(startTime, endTime)) {
-      toReturn =  event;
+      eventsInDateRange.push(event);
     }
   });
 
-  return toReturn;
+  eventsInDateRange.sort(function(a, b) {
+    var aDate = a.getPropertyValue('DTSTART')
+    var bDate = b.getPropertyValue('DTSTART')
+    if (aDate.getHours() == bDate.getHours()) {
+       return aDate.getMinutes() - bDate.getMinutes();
+    }
+
+    return aDate.getHours() - bDate.getHours();
+  });
+
+  return eventsInDateRange;
 }
+
 
 module.exports = router;
